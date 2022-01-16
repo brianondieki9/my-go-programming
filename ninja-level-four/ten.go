60685c05ad597d48f7d44670c8cfb6b6ca335c91
Brian Ondieki <brianondieki@Brians-MBP-2.attlocal.net>
2022-01-16 12:48:05 -0600

    added excercise four assignment

--- ninja-level-four/ten.go
+++ ninja-level-four/ten.go
@@ -1,1 +1,31 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	m := make(map[string][]string)
+
+	first_person := []string{`bond_james`, `Shaken, not stirred`, `Martinis`, `Women`}
+	second_person := []string{`moneypenny_miss`, `James Bond`, `Literature`, `Computer Science`}
+	third_person := []string{`no_dr`, `Being evil`, `Ice cream`, `Sunset`}
+
+	m["peter"] = first_person
+	m["black"] = second_person
+	m["brown"] = third_person
+
+	//add a new record to the map
+	m["jones"] = []string{`alexis sachez`, `meat stew`, `Science Fiction`, `Soccer`}
+
+	//delete a record from the map and then print the map
+	delete(m, "brown")
+	delete(m, "peter")
+
+	//print the map
+	for k, v := range m {
+		fmt.Println(k, v)
+		for i, val := range v {
+			fmt.Println(i, val)
+		}
+	}
+}
